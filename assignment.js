44a8d69ac492619a70243ad438699ecf96835c54
Hasnain Wakeel <wakeelanjum112@gmail.com>
2025-08-29 07:49:53 +0500

    Formatted Code of Javascript .

--- assignment.js
+++ assignment.js
@@ -4,14 +4,9 @@
 
 alert("Hello World")
 
-
-
-
 // Ex no 2 
 
 alert("Hasnain Raza")
-
-
 
 
 // Ex no 3
@@ -19,14 +14,10 @@ alert("Hasnain Raza")
 alert(12)
 
 
-
-
 // Ex no 4
 
 var message = "My name is Muhammad Hasnain Raza"
 alert(message)
-
-
 
 
 // Ex no 5
@@ -51,13 +42,11 @@ userName = "Muhammad Hasnain Raza"
 console.log(userName)
 
 
-
-
-
 // Ex no 2 
 
 var Greet = "Welcome Hasnain Raza"
 alert(Greet)
+
 
 // Ex no 3 
 
@@ -66,10 +55,12 @@ var lastName = "Hasnain Raza"
 var fullName = firstName + lastName
 console.log(fullName)
 
+
 // Ex no 4
 
 var quote_1 = "Practice Makes A Person Perfect"
 console.log(quote_1)
+
 
 // Ex no 5 
 
@@ -77,20 +68,27 @@ var city = "Karachi"
 var greeting = "Welcome to " + city
 console.log(greeting)
 
+
+
 // Topic 3 : Variables For Numbers ------------------------
+
 
 // Ex no 1 
 
 var age;
 age = 17
 console.log(age)
- 
+
+
+
 // Ex no 2 
 
 var num1 = 11
 var num2 = 12
 var sum = num1 + num2
 console.log(sum)
+
+
 
 // Ex no 3 
 
@@ -99,6 +97,8 @@ var num2 = 17
 var sub = num1 - num2
 console.log(sub)
 
+
+
 // Ex no 4
 
 var num1 = 6
@@ -106,12 +106,19 @@ var num2 = 6
 var multiply = num1 * num2
 console.log(multiply)
 
+
+
 // Ex no 5
 
 var yourAge = Number(prompt("Enter Your Age"))
 console.log("Your Age is " + yourAge)
 
+
+
+
 // Topic 4 : Maths Expressions ----------------------------
+
+
 
 // Ex no 1
 
@@ -120,11 +127,14 @@ var y = 5
 var z = x + y
 console.log(z)
 
+
+
 // Ex no 2
 
 var x = 10
 var y = 5
 alert(y - x)
+
 
 
 // Ex no 3
@@ -133,11 +143,15 @@ var x = 10
 var y = 5
 console.log(x * y)
 
+
+
 // Ex no 4
 
 var x = 10
 var y = 5
 console.log(x / y)
+
+
 
 // Ex no 5
 
@@ -160,12 +174,10 @@ alert("Welcome " + Name)
 
 
 
-
 // Ex no 2
 
 var ageOfUser = prompt("Enter Your Age")
 alert("You are " + ageOfUser + " Years Old ")
-
 
 
 // Ex no 3
@@ -175,16 +187,21 @@ var DoubledNumber = inputNumber * 2
 console.log("The Double Of " + inputNumber + " Is " +  DoubledNumber)
 
 
+
 // Ex no 4
 
 var Fav_Color = prompt("Enter Your Favourite Color")
 console.log("Wow! I also like " + Fav_Color + " Color")
 
 
+
 // Ex no 5 
 
 var CityName = prompt("Enter Your City Name")
 console.log("You are from " + CityName)
+
+
+
 
 // Topic 6 : Conditional Statements and Comparison Operators -------
 
@@ -228,7 +245,6 @@ else {
 }
 
 
-
 // Ex no 4
 
 // var password = 12345
@@ -268,4 +284,5 @@ console.log(value1 === value2);  // false
 console.log(value1 != value2);   // false
 
 // Strict inequality (!==): compares value and type
+
 console.log(value1 !== value2);  // true
